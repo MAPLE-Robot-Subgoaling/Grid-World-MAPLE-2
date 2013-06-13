5c394c03c03b941198acb8a93be5949493c694c2
Bhuvana Bellala <bbella1@umbc.edu>
2013-06-13 18:18:35 -0400

    Added setters and getters for the action class attributes

--- GridWorld-2/src/main/Action.java
+++ GridWorld-2/src/main/Action.java
@@ -2,8 +2,19 @@ package main;
 
 import java.util.HashMap;
 
+/**
+ * 
+ * @author Bhuvana Bellala; Tenji Tembo
+ * Class Details:
+ * 	This class is designed to contain the action for each state
+ * Attributes:
+ * 	probabilites: still needs to finalized as to hold whether a string and a double
+ * or an action and double;
+ * 	For now string sounds better because with action comes extra complexity
+ */
+
 public class Action {
-	
+
 	private String name;
 	protected HashMap<String, Double> probablities;
 	//if this particular action is taken at the given
@@ -26,5 +37,29 @@ public class Action {
 		this.endState = other.endState;
 		this.qValue = other.qValue;
 		this.probablities = other.probablities;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Node getEndState() {
+		return endState;
+	}
+
+	public void setEndState(Node endState) {
+		this.endState = endState;
+	}
+
+	public double getqValue() {
+		return qValue;
+	}
+
+	public void setqValue(double qValue) {
+		this.qValue = qValue;
 	}
 }
