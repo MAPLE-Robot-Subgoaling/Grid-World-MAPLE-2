eaa6757f74eb9159d5192e4111e66ab7e79a86fc
Bhuvana Bellala <bbella1@umbc.edu>
2013-06-13 17:46:15 -0400

    Finished the basic node class

--- GridWorld-2/src/main/Node.java
+++ GridWorld-2/src/main/Node.java
@@ -2,14 +2,25 @@ package main;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author Bhuvana Bellala
+ * Class Details:
+ * 	This class is designed to contain the Node for the Graph ADT
+ * Attributes:
+ * 	listOfActions - Action objects that can be carried out in a state
+ */
+
 public class Node {
 	
+	//list of attributes for the class
 	private ArrayList<Action> listOfActions;
 	private int reward;
 	private String name;
 	private boolean isHere;
 	private boolean isGoal;
 	
+	//Constructor: Initializes the attributes
 	public Node(String name, int reward){
 		
 		this.name = name;
@@ -18,6 +29,47 @@ public class Node {
 		this.isGoal = false;
 		this.listOfActions = new ArrayList<Action>();
 		
+	}
+
+	public int getReward() {
+		return reward;
+	}
+
+	public void setReward(int reward) {
+		this.reward = reward;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isHere() {
+		return isHere;
+	}
+
+	public void setHere(boolean isHere) {
+		this.isHere = isHere;
+	}
+
+	public boolean isGoal() {
+		return isGoal;
+	}
+
+	public void setGoal(boolean isGoal) {
+		this.isGoal = isGoal;
+	}
+	
+	/**toString() - print the node
+	 */
+	public String toString(){
+		
+		String stateInfo = "";
+		stateInfo += "The state is " + this.name;
+		return stateInfo;
 	}
 
 }
