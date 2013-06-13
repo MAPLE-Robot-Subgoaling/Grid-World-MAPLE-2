0c0aacff134b5c4cc24e1edc031401f6d304362d
Bhuvana Bellala <bbella1@umbc.edu>
2013-06-13 18:25:43 -0400

    A new class GraphDomain
    This is where I was planning on creating the actual domain. The driver
    class could act as the environment and could be more general.

---
+++ GridWorld-2/src/main/GraphDomain.java
@@ -0,0 +1,12 @@
+package main;
+
+//Create the actual domain
+//Create the states(Node) and associate with actions
+public class GraphDomain {
+	
+	//Constructor: create the 9 states and the associated actions
+	public GraphDomain(){
+		
+	}
+
+}
