f12848e63cf3137469861953c1200a55c871df00
Tenji Tembo <us74092@umbc.edu>
2013-06-19 09:43:52 -0400

    Updated Driver to Run until Agent Reaches Goal
    Final checks and commits are here for this project.
    We should be good to go.

--- GridWorld-2/src/main/Driver.java
+++ GridWorld-2/src/main/Driver.java
@@ -8,9 +8,17 @@ public class Driver {
 		 */
 
 		GraphDomain graph = new GraphDomain();
-		for(int i = 0; i <= 10; i++){
-			graph.move(graph.findAgent());
+		for(int i = 0; i <= 3; i++){ //Runs three trials
+			System.out.println("Episode Number: " + i + "\n");
+			int step = 0;
+			
+			while(!graph.isWinner(graph.findAgent())){
+				System.out.println("Step #: " + step);
+				graph.move(graph.findAgent());
+				step++;
+			}
+			System.out.println("\nThe Agent has reached the goal state. Number of steps: " + step + "\n");
+			graph.resetSim();
 		}
 	}
-
 }
